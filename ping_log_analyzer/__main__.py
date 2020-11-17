77ea642d275e9558a6ad3fff87a66a2801f8308c
Masaya Oura <mairu0514m.o@gmail,com>
2020-11-18 00:52:00 +0900

    存在しないタスク選択時にエラーを出力

--- ping_log_analyzer/__main__.py
+++ ping_log_analyzer/__main__.py
@@ -1,13 +1,17 @@
 import argparse
+import sys
 
 from . import failure_period
 
+# 引数の処理
 parser = argparse.ArgumentParser()
 parser.add_argument('log', help='ログファイルのパス')
 args = parser.parse_args()
 
+# ログファイルのパスの取得
 log_file_path = args.log
 
+# Taskの選択
 print('1. Failure period\n'
       '2. Failure period after a certain number of timeouts\n'
       '3. Overload condition Period\n'
@@ -28,4 +32,4 @@ elif task_num == 5:
     # exit
     pass
 else:
-    print('Please input 1')
+    print('Error: Please input 1~5', file=sys.stderr)
