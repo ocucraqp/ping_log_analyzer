8acfe8a083c7951482c4af3d132d11fbaf966377
Masaya Oura <mairu0514m.o@gmail,com>
2020-11-18 03:55:52 +0900

    入力をパラメータに統一

--- ping_log_analyzer/__main__.py
+++ ping_log_analyzer/__main__.py
@@ -2,33 +2,34 @@ import sys
 
 from . import failure_period, get_args
 
-# ログファイルのパスの取得
+# パラメータの取得
 args = get_args.get_args()
 log_file_path = args.log
+task = int(args.task)
 
-# Taskの選択
-print('1. Failure period\n'
-      '2. Failure period after a certain number of timeouts\n'
-      '3. Overload condition Period\n'
-      '4. Network failure period for each subnet\n'
-      '5. Exit\n'
-      '\n')
-task_num = int(input('Enter the number of things you want to output: '))
-
-if task_num == 1:
+if task == 1:
     failure_period.output_failure_period(log_file_path)
-elif task_num == 2:
-    N = int(input('Enter indicators of failure: '))
-    failure_period.output_failure_period(log_file_path, N=N)
-elif task_num == 3:
-    N = int(input('Enter indicators of failure: '))
-    m = int(input('Enter number of response times to account for overloads: '))
-    t = int(input('Enter the average response time to be overloaded: '))
-    failure_period.output_failure_period(log_file_path, m=m, t=t)
-elif task_num == 4:
-    N = int(input('Enter indicators of failure: '))
-    failure_period.output_failure_period(log_file_path, N=N, network_flg=True)
-elif task_num == 5:
+elif task == 2:
+    try:
+        N = int(args.N)
+        failure_period.output_failure_period(log_file_path, N=N)
+    except TypeError as e:
+        print('Error: Please input N', file=sys.stderr)
+elif task == 3:
+    try:
+        N = int(args.N)
+        m = int(args.m)
+        t = int(args.t)
+        failure_period.output_failure_period(log_file_path, m=m, t=t)
+    except TypeError as e:
+        print('Error: Please input N, m, t', file=sys.stderr)
+elif task == 4:
+    try:
+        N = int(args.N)
+        failure_period.output_failure_period(log_file_path, N=N, network_flg=True)
+    except TypeError as e:
+        print('Error: Please input N', file=sys.stderr)
+elif task == 5:
     # exit
     pass
 else:
